db1fa648094ce7102a084923f6429ae8d47f7805
michoel kahana <kmichoel25@gmail.com>
2022-11-12 21:46:30 -0500

    commit

---
+++ index.js
@@ -0,0 +1,51 @@
+/*function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+  sayHelloToGuadalupe()
+  sayHelloToLiz()
+  sayHelloToSamip()
+  function doSomething(thing) {
+    console.log(thing);
+  }
+  
+  doSomething("Bananas are better than Apples"); // passing the argument 'anything' into our function 
+  function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+  
+  sayHelloTo("Guadalupe"); // "Hello, Guadalupe!"
+  sayHelloTo("Jane"); // "Hello, Jane!"
+  sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+  sayHelloTo(1); // "Hello, 1!"
+  
+  // ^ Note that in the above, JavaScript coerces the number 1 to the string "1"
+  function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Julio", "Hello");
+  function add(x, y) {
+    return x + y;
+  }
+  console.log(add(1,2));
+  /*function say(greeting, firstName) {
+    console.log (`${greeting}, ${firstName}!`);
+  }
+  console.log(say("Hello", "Liz"))
+  function say(greeting, firstName)
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+    
+  }
+  console.log(say("Howdy", "partner"));*/
+function say(winning, basketballTeam) {
+    console.log(`${basketballTeam}, ${winning}`)
+}
+say("Warriors ","Losing ")
